c0f81ed0e0e9f07521af7cf457bf6bd037e60e96
Asteway Nigussie <asteway17@gmail.com>
2025-05-19 20:09:16 -0500

    added code to Portfolio class in Portfolio app

--- Portfolio/src/main/java/com/pluralsight/Portfolio.java
+++ Portfolio/src/main/java/com/pluralsight/Portfolio.java
@@ -1,4 +1,54 @@
 package com.pluralsight;
 
+import com.pluralsight.finance.Valuable;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Portfolio {
+
+    private String name;
+    private String owner;
+    private List<Valuable> assets;
+
+    public Portfolio(String name, String owner) {
+        this.name = name;
+        this.owner = owner;
+        this.assets = new ArrayList<>();
+    }
+
+    public void add(Valuable asset) {
+        assets.add(asset);
+    }
+
+    public double getValue() {
+        double netValue = 0;
+        for (Valuable asset : assets) {
+            netValue += asset.getValue();
+        }
+        return netValue;
+    }
+
+    public Valuable getMostValuable() {
+        if (assets.isEmpty())
+            return null;
+        Valuable mostValuable = assets.get(0);
+        for (Valuable asset : assets) {
+            if (asset.getValue() > mostValuable.getValue()) {
+                mostValuable = asset;
+            }
+        }
+        return mostValuable;
+    }
+
+    public Valuable getLeastValuable() {
+        if (assets.isEmpty())
+            return null;
+        Valuable leastValuable = assets.get(0);
+        for (Valuable asset : assets) {
+            if (asset.getValue() < leastValuable.getValue()) {
+                leastValuable = asset;
+            }
+        }
+        return leastValuable;
+    }
 }
