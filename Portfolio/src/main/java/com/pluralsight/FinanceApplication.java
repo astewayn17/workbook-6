40bc8141471bbf4025f2c156cce9fbd44b17073e
Asteway Nigussie <asteway17@gmail.com>
2025-05-21 10:44:49 -0500

    minor change to main app

--- Portfolio/src/main/java/com/pluralsight/FinanceApplication.java
+++ Portfolio/src/main/java/com/pluralsight/FinanceApplication.java
@@ -10,7 +10,7 @@ public class FinanceApplication {
         BankAccount account1 = new BankAccount(123, "Pam", 12500);
         Valuable account2 = new BankAccount(456, "Gary", 1500);
         // try to deposit money into both accounts
-        account1.deposit(100);
+        //account1.deposit(100);
         //account2.deposit(100);
 
     }
--- Portfolio/src/main/java/com/pluralsight/Portfolio.java
+++ Portfolio/src/main/java/com/pluralsight/Portfolio.java
@@ -2,7 +2,6 @@ package com.pluralsight;
 
 import com.pluralsight.finance.Valuable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
